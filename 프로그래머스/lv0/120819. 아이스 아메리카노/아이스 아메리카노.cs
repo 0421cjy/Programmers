8eb8773944e16e7cd7ef6deeb5a638d489a79694
ChoiJinYong <cherryofsnow@gmail.com>
2023-09-27 14:38:55 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.15 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int[] solution(int money) {
+        const int americano = 5500;
+
+        int cup = money / americano;
+        int r = money % americano;
+
+        return new int[] { cup, r};
+    }
+}
