2c08b94eba8a2eee8d22ba0916bfc46d7af517bc
ChoiJinYong <cherryofsnow@gmail.com>
2023-10-07 12:43:00 +0900

    [unrated] Title: 공배수, Time: 0.14 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.cs
+++ 프로그래머스/unrated/181936. 공배수/공배수.cs
@@ -4,10 +4,7 @@ public class Solution {
     public int solution(int number, int n, int m) {
         int answer = 0;
         
-        var r1 = number % n;
-        var r2 = number % m;
-        
-        if (r1 + r2 == 0)
+        if ((number % n) + (number % m) == 0)
         {
             answer = 1;
         }
