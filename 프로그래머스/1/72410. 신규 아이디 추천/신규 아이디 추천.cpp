697bf902202a2b748155e3565e1a732ad15254b7
ChoiJinYong <cherryofsnow@gmail.com>
2023-10-25 14:12:03 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.cpp
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.cpp
@@ -1,39 +1,20 @@
 #include <string>
-#include <vector>
 
 using namespace std;
 
 bool IsValidChar(char c)
 {
-    if ('a' <= c && c <= 'z')
-    {
-        return true;
-    }
-
-    if ('0' <= c && c <= '9')
-    {
-        return true;
-    }
-
-    if (c == '-' || c == '_' || c == '.')
-    {
-        return true;
-    }
+    if ('a' <= c && c <= 'z') return true;
+    if ('0' <= c && c <= '9') return true;
+    if (c == '-' || c == '_' || c == '.') return true;
 
     return false;
 }
 
 string DeleteDot(string str)
 {
-    if (str[0] == '.')
-    {
-        return str.substr(1, str.length());
-    }
-
-    if (str[str.length() - 1] == '.')
-    {
-        return str.substr(0, str.length() - 1);
-    }
+    if (str[0] == '.') return str.substr(1, str.length());
+    if (str[str.length() - 1] == '.') return str.substr(0, str.length() - 1);
 
     return str;
 }
@@ -84,6 +65,7 @@ string solution(string new_id) {
     
     nnewid = DeleteDot(nnewid);
 
+    // 7 step
     if (nnewid.length() < 3)
     {
         char t = nnewid[nnewid.length() - 1];
