40306fb4018a84780ff6e76295c9a64b80cae5cc
ChoiJinYong <cherryofsnow@gmail.com>
2023-11-02 11:00:57 +0900

    [level unrated] Title: 외계행성의 나이, Time: 2.43 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public string solution(int age) {
+        string answer = "";
+        string ageString = age.ToString();
+        
+        Dictionary<int, int> d = new Dictionary<int, int>();
+        int startChar = 97;
+
+        for (int i = 0; i < 25; i++)
+        {
+            d.Add(i, startChar + i);
+        }
+
+        for (int i = 0; i < ageString.Length; i++)
+        {
+            var t = Int32.Parse(ageString[i].ToString());
+            var temp = d[t];
+            answer += Convert.ToChar(temp);
+        }
+        
+        return answer;
+    }
+}
