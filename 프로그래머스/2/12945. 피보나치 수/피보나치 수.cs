bd6c6753604aaa3b156ced6a420291986b2c653a
ChoiJinYong <cherryofsnow@gmail.com>
2023-11-06 10:55:06 +0900

    [level 2] Title: 피보나치 수, Time: 1.40 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int solution(int n) {
+        int[] array = new int[n + 1];
+        int first = 0;
+        int second = 1;
+        
+        array[0] = 0;
+        array[1] = 1;
+        
+        for (int i = 2; i <= n; i++)
+        {
+            array[i] = (array[i - 2] + array[i - 1]) % 1234567;
+        }
+        
+        return array[n];
+    }
+}
