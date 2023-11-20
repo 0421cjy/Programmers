b3adf1bbf8dfba39cbc9f2d3b4dbf86b93ff5389
ChoiJinYong <cherryofsnow@gmail.com>
2023-11-20 17:31:54 +0900

    [level unrated] Title: 접미사인지 확인하기, Time: 0.21 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int solution(string my_string, string is_suffix) {
+        
+        for (int i = 0;  i <= my_string.Length; i++)
+        {
+            if (my_string.Substring(i) == is_suffix)
+            {
+                return 1;
+            }
+        }
+        
+        return 0;
+    }
+}
