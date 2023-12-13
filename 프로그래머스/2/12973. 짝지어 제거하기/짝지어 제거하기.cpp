049ca85efe57846d2f4d05156dfde1c4a19c8d02
ChoiJinYong <cherryofsnow@gmail.com>
2023-12-13 17:19:24 +0900

    [level 2] Title: 짝지어 제거하기, Time: 5.42 ms, Memory: 6.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int solution(string s)
+{
+    stack<char> stack;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (!stack.empty() && stack.top() == s[i])
+        {
+            stack.pop();
+        }
+        else
+        {
+            stack.push(s[i]);
+        }
+    }
+
+    return (stack.empty() ? 1 : 0);
+}
