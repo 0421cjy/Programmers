61d18d46f674ccc0c2cf26e3f67ed95e29f130d9
ChoiJinYong <cherryofsnow@gmail.com>
2023-12-13 11:11:31 +0900

    [level 1] Title: 모의고사, Time: 3.28 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cs
+++ 프로그래머스/1/42840. 모의고사/모의고사.cs
@@ -4,34 +4,9 @@ using System.Linq;
 
 public class Solution {
     public int[] solution(int[] answers) {
-        Queue<int> list1 = new Queue<int>();
-        list1.Enqueue(1);
-        list1.Enqueue(2);
-        list1.Enqueue(3);
-        list1.Enqueue(4);
-        list1.Enqueue(5);
-
-        Queue<int> list2 = new Queue<int>();
-        list2.Enqueue(2);
-        list2.Enqueue(1);
-        list2.Enqueue(2);
-        list2.Enqueue(3);
-        list2.Enqueue(2);
-        list2.Enqueue(4);
-        list2.Enqueue(2);
-        list2.Enqueue(5);
-
-        Queue<int> list3 = new Queue<int>();
-        list3.Enqueue(3);
-        list3.Enqueue(3);
-        list3.Enqueue(1);
-        list3.Enqueue(1);
-        list3.Enqueue(2);
-        list3.Enqueue(2);
-        list3.Enqueue(4);
-        list3.Enqueue(4);
-        list3.Enqueue(5);
-        list3.Enqueue(5);
+        int[] list1 = new int[] { 1, 2, 3, 4, 5 };
+        int[] list2 = new int[] { 2, 1, 2, 3, 2, 4, 2, 5 };
+        int[] list3 = new int[] { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
 
         Dictionary<int, int> dic = new Dictionary<int, int>();
         dic.Add(1, 0);
@@ -40,35 +15,26 @@ public class Solution {
 
         for (int i = 0; i < answers.Length; i++)
         {
-            var item = list1.Dequeue();
-            if (answers[i] == item)
+            if (answers[i] == list1[i % list1.Length])
             {
                 dic[1]++;
             }
-            
-            list1.Enqueue(item);
         }
 
         for (int i = 0; i < answers.Length; i++)
         {
-            var item = list2.Dequeue();
-            if (answers[i] == item)
+            if (answers[i] == list2[i % list2.Length])
             {
                 dic[2]++;
             }
-            
-            list2.Enqueue(item);
         }
 
         for (int i = 0; i < answers.Length; i++)
         {
-            var item = list3.Dequeue();
-            if (answers[i] == item)
+            if (answers[i] == list3[i % list3.Length])
             {
                 dic[3]++;
             }
-            
-            list3.Enqueue(item);
         }
 
         var maxValue = dic.Max(x => x.Value);
