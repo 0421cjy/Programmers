e6b2a681a5f89e21ef9e2a883c8bc94589e4a180
ChoiJinYong <cherryofsnow@gmail.com>
2024-01-05 11:11:53 +0900

    [level 1] Title: 소수 찾기, Time: 182.37 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
@@ -1,9 +1,10 @@
 using System;
 
 public class Solution {
-    public int solution(int n) {
+    public int solution(int n) 
+    {
         int answer = 0;
-
+        
         for (int i = 2; i <= n; i++)
         {
             if (IsPrime(i))
@@ -15,16 +16,18 @@ public class Solution {
         return answer;
     }
     
-    private static bool IsPrime(int n)
+    public bool IsPrime(int n)
     {
-        for (int i = 2; i*i <= n; i++)
+        for (int i = 2; i * i <= n; i++)
         {
+            if (i == n) continue;
+            
             if (n % i == 0)
             {
                 return false;
             }
         }
-
+        
         return true;
     }
 }
