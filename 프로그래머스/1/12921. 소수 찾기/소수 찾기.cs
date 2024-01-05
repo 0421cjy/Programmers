b5d584ff511fc1498a560e75ba36a6a7d58a6495
ChoiJinYong <cherryofsnow@gmail.com>
2024-01-05 11:14:27 +0900

    [level 1] Title: 소수 찾기, Time: 188.45 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
@@ -20,8 +20,6 @@ public class Solution {
     {
         for (int i = 2; i * i <= n; i++)
         {
-            if (i == n) continue;
-            
             if (n % i == 0)
             {
                 return false;
