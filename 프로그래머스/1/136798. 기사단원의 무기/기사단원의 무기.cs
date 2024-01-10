07a8588597e89e236ff7963a775c810468343ca1
ChoiJinYong <cherryofsnow@gmail.com>
2024-01-10 12:06:41 +0900

    [level 1] Title: 기사단원의 무기, Time: 1.76 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cs
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cs
@@ -6,33 +6,24 @@ public class Solution {
     public int solution(int number, int limit, int power) {
 
         var powerList = new List<int>();
-        var list = new List<int>();
 
         for (int i = 1; i <= number; i++) 
         {
-            for (int j = 1; j <= i / 2; j++)
-            {
-                if (i % j == 0)
-                {
-                    if (!list.Contains(i))
-                    {
-                        list.Add(i);
-                    }
+            var count = 0;
 
-                    if (!list.Contains(j))
-                    {
-                        list.Add(j);
-                    }
+            for (int j = 1; j * j <= i; j++)
+            {
+                if (j * j == i)
+                {
+                    count++;
+                }
+                else if (i % j == 0)
+                {
+                    count += 2;
                 }
             }
 
-            if (list.Count == 0)
-            {
-                list.Add(1);
-            }
-
-            powerList.Add(list.Count);
-            list.Clear();
+            powerList.Add(count);
         }
 
         powerList = powerList.Select(p => 
