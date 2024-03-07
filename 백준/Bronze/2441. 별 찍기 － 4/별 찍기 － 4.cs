df8e98070f815a7a59261e2243db85cc87b8de37
ChoiJinYong <cherryofsnow@gmail.com>
2024-03-07 17:30:00 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 492 ms, Memory: 6608 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+        var count = Int32.Parse(input);
+
+        for (int i = 0; i < count; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = count - i; 0 < k; k--)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
