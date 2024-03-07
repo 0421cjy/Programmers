dc0a1a308d0913aec3105ea0be5a7f35770175f0
ChoiJinYong <cherryofsnow@gmail.com>
2024-03-07 11:15:53 +0900

    [Bronze V] Title: A+B - 8, Time: 72 ms, Memory: 6224 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+        var count = Int32.Parse(input);
+
+        for (int i = 0; i < count; i++)
+        {
+            var nums = Console.ReadLine().Split();
+            var num1 = Int32.Parse(nums[0]);
+            var num2 = Int32.Parse(nums[1]);
+
+            Console.WriteLine($"Case #{i + 1}: {num1} + {num2} = {num1 + num2}");
+        }
+    }
+}
