e67def1798a5834568b49065f70b1e1321454619
ChoiJinYong <cherryofsnow@gmail.com>
2024-03-08 10:30:42 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 64 ms, Memory: 5644 KB -BaekjoonHub

--- 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cs
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cs
@@ -1,40 +1,44 @@
 using System;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var input = Console.ReadLine();
+        using var writer = new StreamWriter(Console.OpenStandardOutput());
+        using var reader = new StreamReader(Console.OpenStandardInput());
+
+        var input = reader.ReadLine();
         var count = Int32.Parse(input);
 
         for (int i = 0; i < count; i++)
         {
             for (int j = count - i; 1 < j; j--)
             {
-                Console.Write(" ");
+                writer.Write(" ");
             }
 
             for (int j = 0; j <= i; j++)
             {
-                Console.Write("*");
+                writer.Write("*");
             }
 
-            Console.WriteLine();
+            writer.WriteLine();
         }
 
         for (int i = 1; i < count; i++)
         {
             for (int j = 0; j < i; j++)
             {
-                Console.Write(" ");
+                writer.Write(" ");
             }
 
             for (int j = count - i; 0 < j; j--)
             {
-                Console.Write("*");
+                writer.Write("*");
             }
 
-            Console.WriteLine();
+            writer.WriteLine();
         }
     }
 }
