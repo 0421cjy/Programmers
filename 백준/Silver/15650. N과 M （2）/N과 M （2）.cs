80cea959425fdef1b5261f14ae3cdde27c6657c0
ChoiJinYong <cherryofsnow@gmail.com>
2024-03-18 11:44:02 +0900

    [Silver III] Title: N과 M (2), Time: 80 ms, Memory: 5852 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var sw = new StreamWriter(Console.OpenStandardOutput());
+        var sr = new StreamReader(Console.OpenStandardInput());
+
+        var input = sr.ReadLine().Split();
+        var num1 = Int32.Parse(input[0]);
+        var num2 = Int32.Parse(input[1]);
+
+        var ints = Enumerable.Repeat(0, num1).ToArray();
+        var visit = Enumerable.Repeat(false, num1).ToArray();
+
+        Combination_DFS(sw, ints, 0, 0, num2);
+
+        sw.Close();
+        sr.Close();
+    }
+
+    static void Combination_DFS(StreamWriter sw, int[] array, int now, int pos, int r)
+    {
+        if (now == r)
+        {
+            for (int i = 0; i < r; i++)
+            {
+               sw.Write($"{array[i]} ");
+            }
+
+            sw.WriteLine();
+            return;
+        }
+
+        for (int idx = pos; idx < array.Length; idx++)
+        {
+            array[now] = idx + 1;
+            Combination_DFS(sw, array, now + 1, idx + 1, r);
+        }
+    }
+}
